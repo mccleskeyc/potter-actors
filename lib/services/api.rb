8b4f577b49bda59cf89b34d2cb90cf3b576945b6
mccleskeyc <mccleskeyc@gmail.com>
2020-12-15 18:43:23 -0500

    added comment

--- lib/services/api.rb
+++ lib/services/api.rb
@@ -4,7 +4,7 @@ class Api
     URL = 'http://hp-api.herokuapp.com/api/characters'
     
 
-    def self.get_data
+    def self.get_data # gets and parses the data from the API
         response = RestClient.get(URL)
         data = JSON.parse(response.body)
         data.each do |hash|
