48d233313ee575abb9d8c10e411be1035759c4a8
mccleskeyc <mccleskeyc@gmail.com>
2020-12-15 22:02:22 -0500

    updated element

--- lib/services/api.rb
+++ lib/services/api.rb
@@ -7,8 +7,8 @@ class Api
     def self.get_data # gets and parses the data from the API
         response = RestClient.get(URL)
         data = JSON.parse(response.body)
-        data.each do |hash|
-            Characters.new(hash)
+        data.each do |character|
+            Characters.new(character)
         end
     end
 
